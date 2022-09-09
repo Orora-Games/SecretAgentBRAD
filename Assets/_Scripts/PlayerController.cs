885ebdff3601375d742c085b9729b9e3dedb4e9c
Tor-Arne Sandstrak <tor.a.sandstrak@nord.no>
2022-09-09 14:42:52 +0200

    Got rid of unnecessary default packages. Coarse movement is down.

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public Keyboard keyboard;
+
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = Keyboard.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if(keyboard.wKey.isPressed)
+            {
+                Debug.Log("X: " + transform.position.x + " Z: " + transform.position.z);
+            }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+            //Debug.Log("X: " + transform.position.x + " Z: " + transform.position.z);
+        } 
+    }
+}
