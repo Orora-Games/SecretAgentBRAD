a67e65e945c58a4651ec2662eb9b6eec6ebee369
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-13 00:40:25 +0200

    Eyes on BRAD now triggers all enemies, and timer resets state.

--- Assets/_Scripts/VisionController.cs
+++ Assets/_Scripts/VisionController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class VisionController : MonoBehaviour 
 {
 	private GameObject guardDogObject;
-	public Material DetectedMaterial;
-	public Material UndetectedMaterial;
-	public Material AlertedMaterial;
+	public Material detectedMaterial;
+	public Material undetectedMaterial;
+	public Material alertedMaterial;
+	private GameObject[] toBeAlerted;
+	private DogController dogController;
+
 
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+		guardDogObject = gameObject.transform.parent.gameObject;
+		toBeAlerted = GameObject.FindGameObjectsWithTag( "Enemy" );
+		dogController = guardDogObject.GetComponent<DogController>();
+	}
 
     // Update is called once per frame
     void Update()
@@ -21,32 +26,34 @@ public class VisionController : MonoBehaviour
         
     }
 
+	/* Triggers as player walks into vision of an enemy */
 	private void OnTriggerEnter ( Collider other ) {
-
-		guardDogObject = gameObject.transform.parent.gameObject;
-
 		if ( other.transform.tag == "Player" ) {
-			Debug.Log( "WHEEEE!!!!" );
-			GetComponent<Renderer>().material = DetectedMaterial;
-			guardDogObject.GetComponent<DogController>().alerted = true;
-			//StartCoroutine( Wait(5) );
-			//GetComponent<Renderer>().material = UndetectedMaterial;
+			/* Next line sets material to "detected" (red) */
+			GetComponent<Renderer>().material = detectedMaterial;
+			/* Make this enemy alerted */
+			dogController.alerted = true;
+			/* This enemy claims to see Player */
+			dogController.eyesOnTarget = true;
+			this.AlertEveryone(); // This alerts all enemies, triggers the timers, and movement (soon)
 		}
 	}
+	/* Triggers when player exits enemy vision */
 	void OnTriggerExit ( Collider Other ) {
 		if ( Other.gameObject.tag == "Player" ) {
-
-			GetComponent<Renderer>().material = AlertedMaterial;
+			dogController.eyesOnTarget = false; // This tells the system that this dog no longer sees Player
+			this.AlertEveryone(true); // This alerts all dogs, triggers the timer,skipps detectingVision-check, and movement (soon)
 		}
 	}
-	//IEnumerator Wait (int timeInSeconds) {
-	//	//Print the time of when the function is first called.
-	//	Debug.Log( "Started Coroutine at timestamp : " + Time.time );
 
-	//	//yield on a new YieldInstruction that waits for 5 seconds.
-	//	yield return new WaitForSeconds( timeInSeconds );
-
-	//	//After we have waited 5 seconds print the time again.
-	//	Debug.Log( "Finished Coroutine at timestamp : " + Time.time );
-	//}
+	/* Is used as a shorthand to run BecomeAlerted() on every guardDog. */
+	void AlertEveryone (bool exitTriggered = false) { //exitTriggered is used to skip detectingVision-check.
+		for ( int i = 0; i < toBeAlerted.Length; i++ ) {
+			/* Makes sure the detecting vision cone stays red. (detectingVision) */
+			if ( !exitTriggered && toBeAlerted[ i ].gameObject.GetInstanceID() == guardDogObject.GetInstanceID() )
+				continue;
+			/* Alerts all other GameObjects with Enemy tag. */
+			this.toBeAlerted[ i ].GetComponent<DogController>().BecomeAlerted();
+		}
+	}
 }
