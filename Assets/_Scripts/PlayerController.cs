267b8ddd121cfe1cb2d0bd6fa9ee79b109919428
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-13 22:09:17 +0200

    Enemies partially follow you, bugsquash, bugsquash,

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,15 +8,17 @@ public class PlayerController : MonoBehaviour
     public float speed = 6f; 
     public float turnSmoothTime = 0.1f;
     public ArrayList  Materials;
+    private float startHeight;
 
-	private GameObject guardDogObject;
+    private GameObject guardDogObject;
 
     float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        startHeight = gameObject.transform.position.y;
+
     }
 
     // Update is called once per frame
@@ -27,9 +29,15 @@ public class PlayerController : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        /*This next one is supposed to move Player back to the ground if they manage to bug themselves to a floating position. */
+        if (Mathf.Abs(transform.position.y - startHeight) >= 0.001)
+        {
+            transform.position = new Vector3(transform.position.x, startHeight, transform.position.z);
+        }
+        /* This block makes Player Turn the way they're moving. */
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg - 90; // Remove when new model comes in.
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
