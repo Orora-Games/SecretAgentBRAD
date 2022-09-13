7d5be3539754111ff6837d21b4ad5d45d3558154
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-13 22:09:17 +0200

    Enemies partially follow you, bugsquash, bugsquash,

--- Assets/_Scripts/DogController.cs
+++ Assets/_Scripts/DogController.cs
@@ -8,9 +8,15 @@ public class DogController : MonoBehaviour
     public bool alerted = false;
     public float alertedTimer = 5.0f;
     public bool eyesOnTarget = false;
+    public float turnSmoothTime = 0.1f;
+    private Vector3 targetPosition;
     private GameObject exclamation;
     private GameObject visionLine;
-    
+    private Vector3 startPosition;
+    private Vector3 lookRotationVector;
+    private Quaternion spawnRotation;
+    public UnityEngine.AI.NavMeshAgent agent;
+
     //Materials
     private Material visionAlertedMaterial;
     private Material visionUndetectedMaterial;
@@ -18,6 +24,8 @@ public class DogController : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
+        startPosition = transform.position;
+        spawnRotation = transform.rotation;
         exclamation = gameObject.transform.Find( "Exclamation" ).gameObject;
 		visionLine = gameObject.transform.Find( "VisionLine" ).gameObject;
 		visionAlertedMaterial = visionLine.GetComponent<VisionController>().alertedMaterial;
@@ -27,22 +35,59 @@ public class DogController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (alerted && alertedTimer > 0f) {
+        if (alerted && alertedTimer > 0f ) {
             alertedTimer = alertedTimer - Time.deltaTime;
 			exclamation.SetActive(true);
-		} else {
+
+            if (eyesOnTarget)
+            {
+                EyesOnTarget();
+                return;
+            }
+            var turnTowardNavSteeringTarget = agent.steeringTarget;
+            Vector3 direction = (turnTowardNavSteeringTarget - transform.position).normalized;
+            lookRotationVector = new Vector3(direction.x, 0, direction.z);
+
+            if (lookRotationVector != Vector3.zero)
+            {
+                Quaternion lookRotation = Quaternion.LookRotation(lookRotationVector);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+            }
+        } else {
             /* The next check checks if this enemy is seeing BRAD now and stops the reset. */
-            if ( eyesOnTarget )	return;
+            if (eyesOnTarget)
+            {
+                EyesOnTarget();
+                return;
+            }
             /* beneath this line resets the DogController and VisionLine's materials */
-			this.visionLine.GetComponent<Renderer>().material = visionUndetectedMaterial;
+            this.visionLine.GetComponent<Renderer>().material = visionUndetectedMaterial;
 			this.alerted = false;
 			exclamation.SetActive(false);
             alertedTimer = 5.0f;
-		}
+            agent.SetDestination(startPosition);
+        }
     }
     public void BecomeAlerted () {
 		this.alerted = true;
-		alertedTimer = 5.0f;
+        targetPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        agent.SetDestination(targetPosition);
+        alertedTimer = 5.0f;
 		this.visionLine.GetComponent<Renderer>().material = visionAlertedMaterial;
 	}
+    private void EyesOnTarget ()
+    {
+        targetPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        agent.SetDestination(targetPosition);
+
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        lookRotationVector = new Vector3(direction.x, 0, direction.z);
+
+        if (lookRotationVector != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(lookRotationVector);
+            Debug.Log("We should be seeing you, " + eyesOnTarget + " // " + lookRotation);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+        }
+    }
 }
