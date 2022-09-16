d0421364d40294b16b7ae15bf4c37ec22640c84a
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-16 14:04:59 +0200

    Fixing #6 Lazy camera now follows Player around

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    /* Source: https://docs.unity3d.com/ScriptReference/Vector3.SmoothDamp.html */
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.position;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.position -= transform.forward * 10; /* Move the camera forwards to avoid clipping through the level */
+    }
+}
+
