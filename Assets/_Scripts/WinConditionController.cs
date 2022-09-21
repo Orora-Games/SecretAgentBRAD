cb688fd68372f084d3df6ebceb199e18fd631e4d
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-21 23:39:24 +0200

    Added Win-condition, respawn-controller, TestScene for second scene, made changes to prefabs

---
+++ Assets/_Scripts/WinConditionController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinConditionController : MonoBehaviour
+{
+    public string nextScene;
+
+	private void Start () {
+		gameObject.GetComponent<Collider>().isTrigger = true;
+
+		if (nextScene.Length == 0) {
+			Debug.LogError( "The next scene has not been defined. Please define Next Scene on \"" + gameObject.name + " \"");
+			return;
+		}
+	}
+
+	private void OnTriggerEnter ( Collider other ) {
+		if ( other.transform.tag == "Player" ) {
+			SceneManager.LoadScene( nextScene );
+		}
+	}
+}
