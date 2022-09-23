6372556801f10332eb5911151896e88bdf658db7
TobyTheProYo <43166568+TobyTheProYo@users.noreply.github.com>
2022-09-23 08:51:54 +0200

    Added two levels
    I added two levels, one is unfinished (level 2) and one semi-finished (needs more testing).

--- Assets/_Scripts/EndController.cs
+++ Assets/_Scripts/EndController.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class QuitController : MonoBehaviour
+public class EndController : MonoBehaviour
 {
+    public float anykeyTimer = 2f;
+
+    private void Start()
+    {
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
         {
             Application.Quit();
         }
-        else if (Input.anyKey)
+        else if (anykeyTimer < 0f && Input.anyKey)
         {
             SceneManager.LoadScene("PrototypeScene");
         }
+        anykeyTimer -= Time.deltaTime;
     }
 }
