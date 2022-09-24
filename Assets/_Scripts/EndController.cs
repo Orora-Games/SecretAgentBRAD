7b2c1be63ebf69f962145fc7f050335ce096f45c
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-24 21:09:10 +0200

    Fixes #21; You can now send people where you want using the exfiltration zones, if you toggle the Ignore Intel boolean. Exfil texture removed.

--- Assets/_Scripts/EndController.cs
+++ Assets/_Scripts/EndController.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndController : MonoBehaviour
-{
-    public float anykeyTimer = 1f;
-    public string loadScene;
-    private void Start()
-    {
-    }
+public class EndController : MonoBehaviour {
+	public float anykeyTimer = 1f;
+	public string loadScene;
+	private void Start () {
+	}
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-        else if (anykeyTimer < 0f && Input.anyKey)
-        {
-            SceneManager.LoadScene( loadScene );
-        }
-        anykeyTimer -= Time.deltaTime;
-    }
+	void Update () {
+		if ( Input.GetKey( KeyCode.Escape ) ) {
+			Application.Quit();
+		} else if ( anykeyTimer < 0f && Input.anyKey ) {
+			SceneManager.LoadScene( loadScene );
+		}
+		anykeyTimer -= Time.deltaTime;
+	}
 }
