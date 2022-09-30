e3a2b0fb3b5770978d82de088161d0cbe325e83b
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-09-30 15:17:40 +0200

    Current MVP GameManager partially Complete

--- Assets/_Scripts/LevelExitController.cs
+++ Assets/_Scripts/LevelExitController.cs
@@ -7,22 +7,11 @@ public class LevelExitController : MonoBehaviour {
 
 	private void Start () {
 		gameObject.GetComponent<Collider>().isTrigger = true;
-
-		/* TODO: Move to Level Manager */
-		if ( nextScene.Length == 0) {
-			Debug.LogError( "The next scene has not been defined. Please define Next Scene on \"" + gameObject.name + " \"");
-			return;
-		}
 	}
 
 	private void OnTriggerEnter ( Collider other ) {
 		if ( other.transform.tag == "Player" ) {
-			/* TODO: Move to Level Manager */
-
-			/* This check makes sure that Player has picked up all intel needed to allow their exfiltration. */
-			if (GameObject.FindGameObjectsWithTag("Intel").Length > 0 && !ignoreIntelState ) return; 
-
-			SceneManager.LoadScene( nextScene );
+			GameManager.Instance.LevelCompleteCheck();
 		}
 	}
 }
