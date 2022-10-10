6ccf696eb2574949371092d601499b3a5b7693a6
Tor-Arne Sandstrak <jobb@tor-arne.com>
2022-10-10 02:15:38 +0200

    Integrated Tobys menus with GameManager scene changing.

--- Assets/_Scripts/IntelController.cs
+++ Assets/_Scripts/IntelController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntelController : MonoBehaviour
-{
+public class IntelController : MonoBehaviour {
 	/* It is used to contain the Exclamation object, to enable/disable when alerted. */
 	private GameObject lock_obj;
 
@@ -12,12 +11,11 @@ public class IntelController : MonoBehaviour
 		gameObject.GetComponent<Collider>().isTrigger = true;
 	}
 
-	private void OnTriggerEnter(Collider other)
-	{
-		if (other.transform.tag == "Player")
-		{
-			if ( !GameManager.Instance ) return;
-			GameManager.Instance.PickedUpIntel(this.gameObject);
+	private void OnTriggerEnter ( Collider other ) {
+		if ( other.transform.tag == "Player") {
+			if ( !GameManager.Instance )
+				return;
+			GameManager.Instance.PickedUpIntel( this.gameObject );
 		}
 	}
 }
