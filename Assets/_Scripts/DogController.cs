51edd2aff7a474c7fad58a4cee31ed5d68e9ee6f
Tor-Arne Sandstrak <jobb@tor-arne.com>
2022-10-12 00:05:27 +0200

    Build settings change

--- Assets/_Scripts/DogController.cs
+++ Assets/_Scripts/DogController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+//[RequireComponent( typeof( NavMeshAgent ) )]
 public class DogController : MonoBehaviour {
 	//Alerted State
 	public bool alerted = false;
