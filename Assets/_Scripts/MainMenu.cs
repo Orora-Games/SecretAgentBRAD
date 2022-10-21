2422a3c175b5051bfcb6c5b69cf1f271ad4f7089
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-10-21 14:16:02 +0200

    Implemented NextLevel Screen

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
+	public GameObject mainMenu, levelSelectMenu, optionsMenu;
 
 	private void Start () {
 		if ( Application.platform == RuntimePlatform.WebGLPlayer ) {
 			GameObject.Find( "quitButton" ).gameObject.SetActive( false );
 		}
+		if ( !GameManager.Instance ) { return;  }
+		
+		if (GameManager.Instance.GetGameState() == GameManager.GameState.LevelSelect) {
+			mainMenu.SetActive( false );
+			levelSelectMenu.SetActive( true );
+		}
+		
 	}
 	//Load Scene
 	public void Play() {
