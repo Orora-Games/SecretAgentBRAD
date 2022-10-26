d33f96252ced222f2fd0f48a91175d9840a3ce45
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-10-26 19:31:44 +0200

    Removed UnityEditor.Progress inclusion.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using static GameManager;
-using static UnityEditor.Progress;
 
 public class GameManager : MonoBehaviour {
 	/* Game Manager tutorial
