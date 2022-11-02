08bb346aacfed5104b16b5abf4d4d6b5cc560181
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-11-02 15:41:05 +0100

    Added Field of View-drawing in editor mode.

--- Assets/_Scripts/FieldOfView.cs
+++ Assets/_Scripts/FieldOfView.cs
@@ -1,7 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
+[ExecuteInEditMode]
 public class FieldOfView : MonoBehaviour {
 	/* Source: Sebastian League Youtube channel
 	 *    Field of view visualisation (E01) https://www.youtube.com/watch?v=rQG9aUWarwE
@@ -240,4 +245,36 @@ public class FieldOfView : MonoBehaviour {
 
 		}
 	}
+
+#if UNITY_EDITOR
+	private void OnDrawGizmos () {
+
+		DrawFieldOfView();
+		///* Get Field of View transform ... */
+		//FieldOfView fov = (FieldOfView)this;
+
+		///* .. Set first drawing colour to white ... */
+		//Handles.color = Color.white;
+
+		///* .. Draw circle around FOV-entity ... */
+		//Handles.DrawWireArc( fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius );
+
+		///* .. Calculate angles for FOV-slice ...*/
+		//Vector3 viewAngleA = fov.DirFromAngle( -fov.viewAngle / 2, false );
+		//Vector3 viewAngleB = fov.DirFromAngle( fov.viewAngle / 2, false );
+
+		///* .. Draw lines out to the circle ... */
+		//Handles.DrawLine( fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius );
+		//Handles.DrawLine( fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius );
+
+		///* .. Change drawing colour to red ... */
+		//Handles.color = Color.white;
+
+		///* .. Go through each of the visibleTargets and ... */
+		//foreach ( Transform visibleTarget in fov.visibleTargets ) {
+		//	/* .. Draw a line to that target. */
+		//	Handles.DrawLine( fov.transform.position, visibleTarget.position );
+		//}
+	}
+#endif
 }
