503a017ac0d30f060774e4c7820aeae18ee6a575
Tor-Arne Sandstrak <togo.danderfluff@gmail.com>
2022-11-21 13:21:57 +0100

    Consistency

--- Assets/_Scripts/Menu/LevelSelector.cs
+++ Assets/_Scripts/Menu/LevelSelector.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class LevelSelector : MonoBehaviour {
 	public string level;
 	public void OpenScene () {
-
-		if ( !GameManager.Instance ) { return; }
+		if ( !GameManager.Instance ) return; 
 		GameManager.Instance.ChangeLevel(level);
 	}
 }
